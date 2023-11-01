620a27ba70b6504a1aaf21f4c515d193cc611bcc
Karl Moody <karlosmoodios@hotmail.co.uk>
2023-11-01 13:16:47 +0000

    Updates, milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,25 +1,57 @@
 import random as r
 import string as s
 
-def random_choice(word_list):
-    word = r.choice(word_list)
+def random_choice(fruit_list):
+    word = r.choice(fruit_list)
     return word
 
-word_list = ['Apple', 'Banana', 'Clementine', 'Date', 'Eggplant']
+fruit_list = [
+    'Apple', 
+    'Banana', 
+    'Clementine', 
+    'Date', 
+    'Eggplant',
+    'Oranges', 
+    'Mango', 
+    'Grapes', 
+    'Strawberry', 
+    'Melon']
+
 letters = [x for x in s.ascii_lowercase]
 
-print(letters)
-print(word_list)
+word = random_choice(fruit_list).lower()
+guessed_word = ''
+turns = round(len(word) * 1.5)
 
-word = random_choice(word_list)
-print(word)
+name = input("\nWelcome to a game of hangman! What is your name? ")
+print (f"Hello {name.capitalize()}! Time to play hangman!")
+print ("Start guessing...")
 
-guess = str(input("Choose a letter..."))
+while turns > 0:
+    failed = 0
 
-if len(guess) == 1 and guess.lower() in letters:
-    if guess.lower() in word.lower():
-        print("Good guess!")
-    else:
-        print("Not present, try again.")
-else:
-    print("Please enter a valid selection. (One letter)")
+    for guess in word:
+        if guess in guessed_word:
+            print(guess, end="" )
+        else:
+            print("_", end="")
+            failed +=1
+    
+    if failed == 0:
+        print(f"\nWell done {name.capitalize()}! You won!")
+        break
+
+    guess = str(input("\nChoose a letter:\t")).lower()
+    
+    if len(guess) != 1 or guess.lower() not in letters: 
+        print("Please enter a single character.")
+        guess = ''
+    
+    elif guess not in word:
+        turns -= 1
+        print(f"\nBad luck! you have {turns} turns remaining.")
+        if turns == 0:
+            print(f"Oh no {name.capitalize()}! You have lost the game!")
+    
+    guessed_word += guess
+    
